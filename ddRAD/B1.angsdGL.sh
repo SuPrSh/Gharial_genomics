996c2090f37d9337a0def31d1b13b27f924b2c06
Surya Sharma <45768049+SuPrSh@users.noreply.github.com>
2026-07-25 16:32:16 -0230

    Add ANGSD script for population structure analysis
    This script runs ANGSD for population structure analysis with specified filters and parameters.

---
+++ ddRAD/B1.angsdGL.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#Note:ANGSD script for population structure analysis with maf filter 
+#---- paths
+DIR="deduplicated.bam"
+OUTDIR="outputfile"
+REF="reference.faa.gz"
+BAMLIST="${DIR}/listBams"
+PREFIX="${OUTDIR}/ggan.filter"
+
+#---- run ANGSD 
+ANGSD="angsd path"
+echo "Filtering..."
+$ANGSD -b "$BAMLIST" -ref "$REF" -out "$PREFIX" \
+  -nThreads "$NCPU" \
+  -remove_bads 1 -only_proper_pairs 1 -baq 1 -uniqueOnly 1 -trim 0 -C 50 \
+  -minMapQ 30 -minQ 30 -minInd 16 -setMinDepthInd 1 -setMaxDepth 84 \
+  -doCounts 1 -skipTriallelic 1 -GL 1 -doMajorMinor 1 -doMaf 1 -minMaf 0.05 -SNP_pval 1e-6 2>&1 | tee "${PREFIX}.run.log"
+echo "Done."
