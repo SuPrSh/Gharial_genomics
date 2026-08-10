70b2f8eb51023fd025126944802ab09633e1efa7
Surya Sharma <45768049+SuPrSh@users.noreply.github.com>
2026-08-10 19:15:21 -0230

    Refactor angsdPCA script for updated paths and settings

--- ddRAD/2.5.angsdPCA.sh
+++ ddRAD/2.5.angsdPCA.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
 # Note: angsd script for genotype calling with posterior probability for PCA
-# -------- resources --------
-NCPU="${1:-24}"   # run: ./run_angsd.sh 32  (optional)
-
 #---- paths
-DIR="deduplicated.bams"
-REF="reference.faa.gz"
+DIR="rmDup"
+REF="reference.fa.gz"
 OUTDIR="angsd/pca"
 SNP="Global_SNPList.txt"
 ANGSD="angsd.path"
-BAMLIST="$DIR/listBams.21samp"
-PREFIX="$OUTDIR/ggan_angsd_pca.r3"
+BAMLIST="$DIR/listBams"
+PREFIX="$OUTDIR/ggan_angsd_pca"
 
 mkdir -p "$OUTDIR"
 
+#----run angsd
 $ANGSD -P "$NCPU" -b "$BAMLIST" -anc "$REF" -out "$PREFIX" \
 	-GL 1 -doGlf 2 -doMajorMinor 3 -doMAF 1 -doPost 1 \
 	-doIBS 1 -doCounts 1 -doCov 1 -makeMatrix 1 -sites "$SNP" \
